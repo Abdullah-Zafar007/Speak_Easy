68ffabe86ac31fd1c3cfe416c135ec12120f1863
Abdullah-Zafar007 <161499653+Abdullah-Zafar007@users.noreply.github.com>
2025-06-28 18:51:37 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-
+// ✅ Add this BEFORE module.exports
+router.get("/test", (req, res) => {
+  res.send("✅ userRoutes working");
+});
 
 module.exports = router;
